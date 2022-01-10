bed0ec1778d6767682bd63504fd0f65c228a0257
OMGerCoder <hughomg@icloud.com>
2022-01-10 19:13:39 +1030

    emergency fix

--- src/cmd/submitVictory.js
+++ src/cmd/submitVictory.js
@@ -26,18 +26,20 @@ module.exports = {
 			if (lvldoc === null) {
 				interaction.reply({content: "That level is not on the list!", ephemeral: true});
 				return;
+			} else {
+				await db.Models.verification.findOne({lvlid: lvlid}, (err, namedoc) => {
+					doc.save();
+				if(process.env.TESTMODE == "TRUE") {
+					interaction.guild.channels.cache.get('923099462337982524').send(`<@&${process.env.LISTTEAM_ROLEID}>\n**${namedoc.lvlname}**\nCompleted by \`${interaction.user.tag}\`\n${videoProof}`);
+				} else {
+					// <@&${process.env.LISTTEAM_ROLEID}> List team ping
+					interaction.guild.channels.cache.get(process.env.RECORDS_CHANNELID).send(`<@&${process.env.LISTTEAM_ROLEID}>\n**${namedoc.lvlname}**\nCompleted by \`${interaction.user.tag}\`\n${videoProof}`);
+				}
+				interaction.reply({content: "Successfully sent completion. List team have been notified", ephemeral: true});
+				})
 			}
 		})
-		await db.Models.verification.findOne({lvlid: lvlid}, (err, namedoc) => {
-			doc.save();
-		if(process.env.TESTMODE == "TRUE") {
-			interaction.guild.channels.cache.get('923099462337982524').send(`<@&${process.env.LISTTEAM_ROLEID}>\n**${namedoc.lvlname}**\nCompleted by \`${interaction.user.tag}\`\n${videoProof}`);
-		} else {
-			// <@&${process.env.LISTTEAM_ROLEID}> List team ping
-			interaction.guild.channels.cache.get(process.env.RECORDS_CHANNELID).send(`<@&${process.env.LISTTEAM_ROLEID}>\n**${namedoc.lvlname}**\nCompleted by \`${interaction.user.tag}\`\n${videoProof}`);
-		}
-		interaction.reply({content: "Successfully sent completion. List team have been notified", ephemeral: true});
-		})
+		
 		
 		
 	},
