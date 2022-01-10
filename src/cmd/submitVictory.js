69b131b584225b2140f7abdecba988c93f6f505c
OMGerCoder <hughomg@icloud.com>
2022-01-10 19:14:10 +1030

    emergency fix 2

--- src/cmd/submitVictory.js
+++ src/cmd/submitVictory.js
@@ -27,7 +27,7 @@ module.exports = {
 				interaction.reply({content: "That level is not on the list!", ephemeral: true});
 				return;
 			} else {
-				await db.Models.verification.findOne({lvlid: lvlid}, (err, namedoc) => {
+				db.Models.verification.findOne({lvlid: lvlid}, (err, namedoc) => {
 					doc.save();
 				if(process.env.TESTMODE == "TRUE") {
 					interaction.guild.channels.cache.get('923099462337982524').send(`<@&${process.env.LISTTEAM_ROLEID}>\n**${namedoc.lvlname}**\nCompleted by \`${interaction.user.tag}\`\n${videoProof}`);
