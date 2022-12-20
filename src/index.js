f50cd295c652d6391aede838c286ad2a24b1af27
OMGer <hugh.stuckless@hotmail.com>
2022-12-20 18:54:23 +1030

    fix placements and move msg

--- src/index.js
+++ src/index.js
@@ -681,6 +681,7 @@ app.post('/api/movelevel/', async(req, res) => {
 										});
 									})
 									listlvl.save();
+									nclguild.channels.cache.get(process.env.LISTUPDATES_CHANNELID).send(`**${vdoc.lvlname}** has been moved to #${newPlacement}.`);
 									res.redirect('/panel');
 								})
 
@@ -700,6 +701,36 @@ app.post('/api/movelevel/', async(req, res) => {
 		res.render('error', {error: 'You are not logged in!', authorized: checkAuthorized(res), info: res.locals.info})
 	}
 })
+
+app.get('/fixplacements', async(req, res) => {
+	const nclguild = await client.guilds.fetch(process.env.GUILDID);
+	if(checkAuthorized(res)) {
+		try {
+			const currentMember = await nclguild.members.fetch(res.locals.info.id)
+			if (currentMember.roles.cache.has('922079625482477599')) {
+				db.Models.listlvl.find({}).sort({placement: 1}).exec(async(err, docs) => {
+					var index = 1;
+					for (const lvl of docs) {
+						lvl.placement = index;
+						await lvl.save();
+						index++;
+					}
+					res.json({msg: 'done'})
+				})
+			} else {
+				res.render('error', {error: 'GET OUT (You are not allowed to access this page)', authorized: checkAuthorized(res), info: res.locals.info})
+			}
+			
+		} catch(err) {
+			res.render('error', {error: 'You are not in our discord server!', authorized: checkAuthorized(res), info: res.locals.info})
+		}
+	} else {
+		res.render('error', {error: 'You are not logged in!', authorized: checkAuthorized(res), info: res.locals.info})
+	}
+	
+
+})
+
 https.createServer({
 	key: privateKey,
 	cert: cert
